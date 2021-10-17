92a8001107ce55148464bb20b676220ffbd007a9
Muttakin Hasib <hasibmolla28@gmail.com>
2021-10-17 13:04:00 +0600

    added input field :zap:

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -7,14 +7,50 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Material(
       color: Colors.white,
-      child: Column(
-        children: [
-          Image.asset("assets/images/login_image.png"),
-          const Text(
-            "Welcome",
-            style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
-          )
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.asset("assets/images/login_image.png", fit: BoxFit.cover),
+            const SizedBox(
+              height: 20.0,
+            ),
+            const Text(
+              "Welcome",
+              style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: const InputDecoration(
+                        hintText: 'Enter username', labelText: 'Username'),
+                  ),
+                  const SizedBox(
+                    height: 20.0,
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                      hintText: 'Enter password',
+                      labelText: 'Password',
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20.0,
+                  ),
+                  ElevatedButton(
+                    onPressed: () => {},
+                    style: TextButton.styleFrom(
+                      minimumSize: const Size(100, 40),
+                    ),
+                    child: const Text('Login'),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
